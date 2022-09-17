861759b7a3f2a8d81563e2208740582758f46d85
Manuel Villing <manuel.villing@gmail.com>
2022-09-17 11:41:42 +0200

    added hardhat

---
+++ packages/hardhart/README.md
@@ -0,0 +1,17 @@
+# hardhat
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
+
+## setup
+
+create a .env file in this folder with following content
+
+```
+GOERLI_PRIVATE_KEY=private key of deployer account
+```
---
+++ packages/hardhart/hardhat.config.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY]
+    }
+  }
+};
